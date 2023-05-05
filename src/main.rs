2751415e98a45b38611b7d81e4256d04e56aab8e
scurtis <sean.curtis@gmail.com>
2023-05-05 15:32:56 -0400

    added guessing logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world, testing git pat!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
+
+        println!("Guess the number!");
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        //parse input to u32
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue, 
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small..."),
+            Ordering::Greater => println!("Too big..."),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+
+    }
+    
+    
+
 }
